27d409682f2f09f7ed83633515222774d2fa0564
achyutkneupane <achyutkneupane@gmail.com>
2026-01-09 20:19:54 +0545

    improvements: enhances LaravelNews facade with post method documentation

--- src/Facades/LaravelNews.php
+++ src/Facades/LaravelNews.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace AchyutN\LaravelNews\Facades;
 
+use AchyutN\LaravelNews\Data\Link;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static Link post(Link $link)
+ *
+ * @see \AchyutN\LaravelNews\LaravelNews
+ */
 final class LaravelNews extends Facade
 {
+    /**
+     * {@inheritDoc}
+     */
     protected static function getFacadeAccessor(): string
     {
         return \AchyutN\LaravelNews\LaravelNews::class;
